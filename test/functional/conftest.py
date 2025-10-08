3ec443dfad4906d779c6c2a196e4032af6c546ee
Edgar Menezes <edgar.menezescosta@zf.com>
2025-10-08 22:27:20 +0200

    Try to run test_basic.py

--- test/functional/conftest.py
+++ test/functional/conftest.py
@@ -4,12 +4,12 @@ from pathlib import Path
 
 @pytest.fixture(scope="session")
 def xilEnvInstallationPath() -> OpenXilEnv:
-    xilEnvInstallationPath = Path("C:\\app\\tools\\openxilenv")
+    xilEnvInstallationPath = Path("d:\\a\\openxilenv\\openxilenv\\OpenXilEnv-Windows-Qt6_9_2")
     assert xilEnvInstallationPath.is_dir(), f"no such directory {xilEnvInstallationPath}"
     return xilEnvInstallationPath
 
 @pytest.fixture(scope="session")
 def iniFilePath() -> Path:
-    iniFilePath = Path("C:\\UserData\\git_ws\\openxilenv\\Samples\\Configurations\\ElectricCarSample.ini")
+    iniFilePath = Path("d:\\a\\openxilenv\\openxilenv\\openxilenv\\Samples\\Configurations\\ElectricCarSample.ini")
     assert iniFilePath.is_file(), f"no such directory {iniFilePath}"
     return iniFilePath
